c39e78a09008fbf835346cd74f2b43cd1ba9c687
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2023-04-07 19:17:13 -0400

    Fixed Toggle DesktopMenu

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const nav_email = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+nav_email.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+  desktopMenu.classList.toggle('inactive');
+}
