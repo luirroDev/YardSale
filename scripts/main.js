da455a35344d40e2eaa068bf8aa553c97a90de05
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2023-04-07 20:00:56 -0400

    Added Toggle Mobile-Menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
 const nav_email = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMobileMenu = document.querySelector('.burguerMenu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 nav_email.addEventListener('click', toggleDesktopMenu);
+burguerMobileMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
 }
